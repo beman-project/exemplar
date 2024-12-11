b93b070212489fd92a39539bb7c3e49cacd9447c
River <26424577+wusatosi@users.noreply.github.com>
2024-12-11 18:45:47 -0500

    add msvc toolchain

---
+++ cmake/msvc-toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_C_COMPILER cl)
+set(CMAKE_CXX_COMPILER cl)
+
+if(BEMAN_BUILDSYS_SANITIZER STREQUAL "ASan")
+    set(CMAKE_CXX_FLAGS_DEBUG_INIT "/fsanitize=address /Zi")
+    set(CMAKE_C_FLAGS_DEBUG_INIT "/fsanitize=address /Zi")
+endif()
+
+set(CMAKE_CXX_FLAGS_DEBUG_INIT
+    "${CMAKE_CXX_FLAGS_DEBUG_INIT} /EHsc /permissive-"
+)
+set(CMAKE_C_FLAGS_DEBUG_INIT "${CMAKE_C_FLAGS_DEBUG_INIT} /EHsc /permissive-")
+
+set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "/EHsc /permissive- /O2")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/EHsc /permissive- /O2")
