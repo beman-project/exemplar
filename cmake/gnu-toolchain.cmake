f7e1c9616edd6a4f437ddcad307d86fb58dc7f4c
River <26424577+wusatosi@users.noreply.github.com>
2024-12-20 15:50:45 -0500

    apply sanitizer to all targets

--- cmake/gnu-toolchain.cmake
+++ cmake/gnu-toolchain.cmake
@@ -6,16 +6,15 @@ set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 
 if(BEMAN_BUILDSYS_SANITIZER STREQUAL "ASan")
-    set(CMAKE_CXX_FLAGS_DEBUG_INIT
-        "-fsanitize=address -fsanitize=leak -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined"
-    )
-    set(CMAKE_C_FLAGS_DEBUG_INIT
+    set(SANITIZER_FLAGS
         "-fsanitize=address -fsanitize=leak -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined"
     )
 elseif(BEMAN_BUILDSYS_SANITIZER STREQUAL "TSan")
-    set(CMAKE_CXX_FLAGS_DEBUG_INIT "-fsanitize=thread")
-    set(CMAKE_C_FLAGS_DEBUG_INIT "-fsanitize=thread")
+    set(SANITIZER_FLAGS "-fsanitize=thread")
 endif()
 
-set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-O3")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O3")
+set(CMAKE_C_FLAGS_DEBUG_INIT "${SANITIZER_FLAGS}")
+set(CMAKE_CXX_FLAGS_DEBUG_INIT "${SANITIZER_FLAGS}")
+
+set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-O3 ${SANITIZER_FLAGS}")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O3 ${SANITIZER_FLAGS}")
--- cmake/msvc-toolchain.cmake
+++ cmake/msvc-toolchain.cmake
@@ -6,14 +6,15 @@ set(CMAKE_C_COMPILER cl)
 set(CMAKE_CXX_COMPILER cl)
 
 if(BEMAN_BUILDSYS_SANITIZER STREQUAL "ASan")
-    set(CMAKE_CXX_FLAGS_DEBUG_INIT "/fsanitize=address /Zi")
-    set(CMAKE_C_FLAGS_DEBUG_INIT "/fsanitize=address /Zi")
+    set(SANITIZER_FLAGS "/fsanitize=address /Zi")
 endif()
 
-set(CMAKE_CXX_FLAGS_DEBUG_INIT
-    "${CMAKE_CXX_FLAGS_DEBUG_INIT} /EHsc /permissive-"
-)
-set(CMAKE_C_FLAGS_DEBUG_INIT "${CMAKE_C_FLAGS_DEBUG_INIT} /EHsc /permissive-")
+set(CMAKE_CXX_FLAGS_DEBUG_INIT "/EHsc /permissive- ${SANITIZER_FLAGS}")
+set(CMAKE_C_FLAGS_DEBUG_INIT "/EHsc /permissive- ${SANITIZER_FLAGS}")
 
-set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "/EHsc /permissive- /O2")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/EHsc /permissive- /O2")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT
+    "/EHsc /permissive- /O2 ${SANITIZER_FLAGS}"
+)
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT
+    "/EHsc /permissive- /O2 ${SANITIZER_FLAGS}"
+)
